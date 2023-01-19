f7c2cda6cc2127154a4bbea7856f725942b25604
delguanzon <yodelguanzon@gmail.com>
2023-01-18 16:24:38 -0800

    Implement Pagination

--- CretaceousApi/Controllers/AnimalsController.cs
+++ CretaceousApi/Controllers/AnimalsController.cs
@@ -17,9 +17,28 @@ namespace CretaceousApi.Controllers
 
     // GET api/animals
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Animal>>> Get()
+    public async Task<List<Animal>> Get(string species, string name, int minimumAge, int? pageNumber, int? pageSize)
     {
-      return await _db.Animals.ToListAsync();
+      IQueryable<Animal> query = _db.Animals.AsQueryable();
+
+      if (species != null)
+      {
+        query = query.Where(entry => entry.Species == species);
+      }
+
+      if (name != null)
+      {
+        query = query.Where(entry => entry.Name == name);
+      }
+
+      if (minimumAge > 0)
+      {
+        query = query.Where(entry => entry.Age >= minimumAge);
+      }
+
+      int _pageSize = pageSize ?? 3;
+      //return await query.ToListAsync();     
+      return await PaginatedList<Animal>.CreateAsync(query, pageNumber ?? 1, _pageSize > 3 ?  3 : _pageSize);
     }
 
     // GET: api/Animals/5
@@ -44,6 +63,57 @@ namespace CretaceousApi.Controllers
       await _db.SaveChangesAsync();
       return CreatedAtAction(nameof(GetAnimal), new { id = animal.AnimalId }, animal);
     }
-    
+
+    // PUT: api/Animals/5
+    // [HttpPut("{id}")]
+    [HttpPut("{id}")]
+    // [Route("/update/{id}")]
+    public async Task<IActionResult> Put(int id, Animal animal)
+    {
+      if (id != animal.AnimalId)
+      {
+        return BadRequest();
+      }
+
+      _db.Animals.Update(animal);
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!AnimalExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+      return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAnimal(int id)
+    {
+      Animal animal = await _db.Animals.FindAsync(id);
+      if (animal == null)
+      {
+        return NotFound();
+      }
+
+      _db.Animals.Remove(animal);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
+    }
+
+    private bool AnimalExists(int id)
+    {
+      return _db.Animals.Any(e => e.AnimalId == id);
+    }
+
   }
 }
